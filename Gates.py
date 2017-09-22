333aada4253888252f31d645cc672bacac64dfef
Leo <leonid.braginsky@gmail.com>
2017-09-22 14:46:33 -0400

    Account for common subexpressions.

--- Gates.py
+++ Gates.py
@@ -41,36 +41,53 @@ def min_circuits(x, inputs=["A", "B"]):
                       (G(g, G(x, a, b), G(y, a, b)) << (a+a+b)
                        for a in (0, 1) for b in (0, 1)))
 
-    x_gates = [(gates_by_name[i], i) for i in x]
-    results = { gates_by_name[i]: (0, i) for i in inputs }
+    x_gates = [gates_by_name[i] for i in x]
+    results = { g: (0, g) for g in (gates_by_name[i] for i in inputs) }
+
+    def count_gates(u, v):
+        c = set()
+        def add(n):
+            w = results[n][1]
+            if type(w) is tuple:
+                c.add(w)
+                g, a, b = w
+                add(a)
+                add(b)
+        add(u)
+        add(v)
+        return len(c) + 1
+    
     while True:
         change = False
         avail = list(results)
-        for g, g_name in x_gates:
-            uv = ((u, v) for u in avail for v in avail)
+        uv = [(u, v) for u in avail for v in avail]
+        for g in x_gates:
             for u, v in uv:
-                nu, cu = results[u]                
-                nv, cv = results[v]
                 w = combine(g, u, v)
-                nw = nu + nv + 1
+                nw = count_gates(u, v)
                 if not w in results or nw < results[w][0]:
-                    results[w] = (nw, (g_name, cu, cv))
+                    results[w] = (nw, (g, u, v))
                     change = True
-        if not change: return results
+        if not change: break
+    return results
+
+def circuit_str(results, i):
+    def cs(i):
+        _size, c = results[i]
+        if not (type(c) is tuple): return gates[c]
+        g, a, b = c
+        return "({} {} {})".format(gates[g], cs(a), cs(b))
+    return cs(i)
 
 if __name__ == '__main__':
     
-    def circuit_str(c):
-        if type(c) is str: return c
-        g, a, b = c
-        return "({} {} {})".format(g, circuit_str(a), circuit_str(b))
-
     def min_circuits_test(x, inputs=["A", "B"]):
         print "\nAll gates from {} {}".format(x, inputs)
         results = min_circuits(x, inputs=inputs)
+
         for g, g_name in sorted(gates.iteritems()):
-            size, circuit = results[g] if g in results else ('-', '-')
-            print "{:10} [{}] {}".format(g_name, size, circuit_str(circuit))
+            size, cs = (results[g][0], circuit_str(results, g)) if g in results else ('-', '-')
+            print "{:10} [{}] {}".format(g_name, size, cs)
 
     min_circuits_test(["Nand"])
     min_circuits_test(["Nand"], inputs=["A", "B", "True", "False"])
